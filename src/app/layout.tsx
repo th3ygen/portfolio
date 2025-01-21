00e0d0445492835c6e3f100b9800d9124dca198e
Aidil <th3ygen@gmail.com>
2025-01-21 21:48:31 +0800

    update seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<ThemeProvider
 					attribute="class"
-					defaultTheme="dark"
+					defaultTheme="system"
 					enableSystem
 				>
 					<Splash />
